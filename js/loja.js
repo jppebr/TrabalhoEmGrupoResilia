cb6c78e5239220696c6dac4f065d7dcbf5a95fff
jppebr <joaopedro1415@gmail.com>
2022-10-08 15:28:33 -0300

    fiz bastante coisas

--- js/loja.js
+++ js/loja.js
@@ -1,2 +1,17 @@
-
-
+function openNav(){
+    document.getElementById('myNav').style.width = '100%'
+    var display = document.getElementById('preto').style.display;
+    if(display == "block")
+    document.getElementById('preto').style.display = 'none';
+    else
+            document.getElementById('preto').style.display = 'block';
+}
+   
+function closeNav(){
+    document.getElementById('myNav').style.width = '0'
+    var display = document.getElementById('preto').style.display;
+        if(display == "none")
+            document.getElementById('preto').style.display = 'block';
+        else
+            document.getElementById('preto').style.display = 'none';
+        }
